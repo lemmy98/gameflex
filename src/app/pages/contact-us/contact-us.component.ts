fe5f68bfd3d6e95113700301cb4ef321634f4d3f
Charles Chukwujekwu Nwoye <charlesnwoye2@gmail.com>
2021-11-11 10:37:49 +0000

    added contact page

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.css']
+  styleUrls: ['./contact-us.component.css', './contact-font.css']
 })
 export class ContactUsComponent implements OnInit {
 
