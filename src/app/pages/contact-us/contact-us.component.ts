200278801cd444fe883ee1c77fe3436fc1202a09
Charles Chukwujekwu Nwoye <charlesnwoye2@gmail.com>
2021-11-15 13:37:01 +0000

    made the fonts on the about and contact pages load with cdn

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.css', './contact-font.css']
+  styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
 
